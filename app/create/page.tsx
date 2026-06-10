368ae96a24da72a6634cf32b01d91faca63591af
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2026-06-10 14:33:55 -0400

    auction create10

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -146,12 +146,12 @@ export default function CreatePage() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white rounded-xl shadow">
-      <h1 className="text-2xl font-bold mb-4">Poster un item</h1>
+    <div className="max-w-2xl mx-auto p-4 bg-white rounded-xl shadow">
+      <h1 className="text-2xl font-bold mb-6">Poster un item</h1>
 
-      <input placeholder="Titre" value={title} onChange={e => setTitle(e.target.value)} className="input" />
 
-      <div className="flex bg-gray-100 p-1 rounded-xl w-full mb-4">
+
+      <div className="flex bg-gray-100 p-1 rounded-xl w-full mb-6">
 
         {/* LOCATION */}
         <button
@@ -190,7 +190,9 @@ export default function CreatePage() {
         </button>
 
       </div>
+       <div className="space-y-5">
 
+      <input placeholder="Titre" value={title} onChange={e => setTitle(e.target.value)} className="input" />
       <textarea placeholder="Description" value={description} onChange={e => setDescription(e.target.value)} className="input" />
 
       <select value={categoryId} onChange={e => setCategoryId(e.target.value)} className="input">
@@ -258,10 +260,10 @@ export default function CreatePage() {
 
       <input placeholder="Ville" value={city} onChange={e => setCity(e.target.value)} className="input" />
       <input placeholder="Adresse" value={address} onChange={e => setAddress(e.target.value)} className="input" />
-
+</div>
       {/* Remplace <input type="file" multiple onChange={handleImages} /> par : */}
 
-      <div className="mt-4">
+      <div className="mt-6">
         <label className="block text-sm font-medium text-gray-700 mb-2">
           Images
         </label>
