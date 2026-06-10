bcca4bf276770dc201b13a96727cfea9ff15501e
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2026-06-10 15:12:35 -0400

    auction create11

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -160,7 +160,7 @@ export default function CreatePage() {
       flex-1 flex items-center justify-center gap-2 px-4 py-2 rounded-lg text-sm font-medium transition-all
       cursor-pointer
       ${type === "RENTAL"
-              ? "bg-white shadow text-blue-600"
+              ? "bg-blue-600 text-white shadow"
               : "text-gray-500 hover:bg-white hover:shadow"}
     `}
         >
@@ -177,7 +177,7 @@ export default function CreatePage() {
               ? "opacity-50 cursor-not-allowed"
               : "cursor-pointer"}
       ${type === "AUCTION"
-              ? "bg-white shadow text-red-500"
+              ? "bg-red-500 text-white shadow"
               : "text-gray-500 hover:bg-white hover:shadow"}
     `}
         >
